54b26f983e4dc791145c9f6ef53db3485054d2e2
idaa355 <idaa355@users.noreply.github.com>
2021-03-25 22:59:38 -0700

    login signup

--- main.py
+++ main.py
@@ -20,5 +20,13 @@ db = SQLAlchemy(app)
 def home():
     return render_template('landing.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port="5001")
