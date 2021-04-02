c155733d38d0778d7ed9b31ecff8f72a5a1f283d
idaa355 <idaa355@users.noreply.github.com>
2021-04-01 22:00:35 -0700

    classes bp

--- classes/app.py
+++ classes/app.py
@@ -3,3 +3,7 @@ from flask import Flask, Blueprint, render_template, flash, redirect, url_for, s
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
+
+classes_bp = Blueprint('classes', __name__,
+                       template_folder='templates',
+                       static_folder='static', static_url_path='assets')
