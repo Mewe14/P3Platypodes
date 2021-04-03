099ca98d6c360d2ddbbd69716ec67c74a71cc9c0
aruls <arul.salaniwal@gmail.com>
2021-04-02 22:42:11 -0700

    blueprint for sports

--- sports/app.py
+++ sports/app.py
@@ -1,5 +1,18 @@
 import os
 from flask import Flask, Blueprint, render_template, flash, redirect, url_for, session, logging
 from flask import request
+from classes.sort import sports
+
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
+
+classes_bp = Blueprint('classes', __name__,
+                       template_folder='templates',
+                       static_folder='static', static_url_path='assets')
+
+@sports_bp.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        return render_template("allsports.html", sports=sports(int(request.form.get("series"))))
+    return render_template("allsports.html", sports=sports(1))
