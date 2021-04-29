5d02013c028d9cc5cd172f685537a27d698e8cd9
crystalwidjaja <pansypig@gmail.com>
2021-04-29 11:06:25 -0700

    bubble sort crystal

--- classes/bubble.py
+++ classes/bubble.py
@@ -1,60 +1,39 @@
-def bubbleSort(arr):
-    n = len(arr)
-    # Traverse through all array elements
-    for i in range(n):
-        swapped = False
-
-        # Last i elements are already
-        #  in place
-        for j in range(0, n-i-1):
-
-            # traverse the array from 0 to
-            # n-i-1. Swap if the element
-            # found is greater than the
-            # next element
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-
-        # IF no two elements were swapped
-        # by inner loop, then break
-        if swapped == False:
-            break
-    print(arr)
-
-
 class BubbleSort:
     """Initializer of class takes series parameter and returns Class Objects"""
     def __init__(self, arr):
         """Built in validation and exception"""
         self._arr = arr
-        self._list = []
-        self._dict = {}
-        self._dictID = 0
         self.ssort()
 
 
     """Algorithm for building book series list, this id called from __init__"""
     def ssort(self):
-        f = bubbleSort(self._arr)
+        n = len(self._arr)
+        # Traverse through all array elements
+        for i in range(n):
+            swapped = False
+
+            # Last i elements are already
+            #  in place
+            for j in range(0, n-i-1):
+
+                # traverse the array from 0 to
+                # n-i-1. Swap if the element
+                # found is greater than the
+                # next element
+                if self._arr[j] > self._arr[j+1] :
+                    self._arr[j], self._arr[j+1] = self._arr[j+1], self._arr[j]
+                    swapped = True
+
+            # IF no two elements were swapped
+            # by inner loop, then break
+            if swapped == False:
+                break
 
         """Getters with decorator to allow . notation access"""
     @property
-    def series(self):
-        return self.ssort
-
-    @property
     def list(self):
         return self._arr
-
-    @property
-    def number(self):
-        return self._list[self._dictID - 1]
-
-    """Traditional Getter requires method access"""
-    def get_sequence(self, nth):
-        return self._dict[nth]
-
 
 
 if __name__ == "__main__":
@@ -62,4 +41,6 @@ if __name__ == "__main__":
 
     '''Constructor of Class object'''
     x = [7, 8, 4, 32, 1, 3]
-    bubbles = BubbleSort(x)
+    bubbles = BubbleSort(x.copy())
+    print(x)
+    print(bubbles.list)
